45832eba60c6886e5d054b5b045a5104ef90c3ba
AMARAIAH PADI <snowflakeonlinetraining001@gmail.com>
2025-08-04 02:41:42 +0000

    commit

--- models/staging/tpch/stg_tpch_line_items.sql
+++ models/staging/tpch/stg_tpch_line_items.sql
@@ -11,7 +11,7 @@ renamed as (
         {{ dbt_utils.surrogate_key(
             ['l_orderkey', 
             'l_linenumber']) }}
-                as order_item_key,
+                as order_item_key, 
         l_orderkey as order_key,
         l_partkey as part_key,
         l_suppkey as supplier_key,
