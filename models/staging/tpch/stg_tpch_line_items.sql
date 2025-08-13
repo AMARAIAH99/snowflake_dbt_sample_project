11ba92b61a7504e3f8a6c738f5eacd0be520c9d6
AMARAIAH PADI <snowflakeonlinetraining001@gmail.com>
2025-08-13 02:26:38 +0000

    commit

--- models/staging/tpch/stg_tpch_line_items.sql
+++ models/staging/tpch/stg_tpch_line_items.sql
@@ -27,7 +27,7 @@ renamed as (
         l_receiptdate as receipt_date,
         l_shipinstruct as ship_instructions,
         l_shipmode as ship_mode,
-        upper_case_name(l_comment) as comment
+        {{upper_case_name('l_comment') }} as comment
 
     from source
 
