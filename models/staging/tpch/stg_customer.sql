8f594ec17b1daa65ece9ec494c367ecdcf184dfd
AMARAIAH PADI <snowflakeonlinetraining001@gmail.com>
2025-10-06 15:34:45 +0000

    commit

--- models/staging/tpch/stg_customer.sql
+++ models/staging/tpch/stg_customer.sql
@@ -1,17 +1,25 @@
-{{ config(
-    materialized = 'table'
-) }}
+with source as (
 
-WITH source_data AS (
-    SELECT 
-    customer_id ,
-    customer_name ,
-    email ,
-    country ,
-    total_purchase ,
-    load_datatimestamp as etl_load_timestamp
-    FROM dev_customerdatalake.customer.customer_data
-    
+    select * from {{ source('tpch', 'orders') }}
+
+),
+
+renamed as (
+
+    select  
+
+        o_orderkey as order_key,
+        o_custkey as customer_key,
+        o_orderstatus as status_code,
+        o_totalprice as total_price,
+        o_orderdate as order_date,
+        o_orderpriority as priority_code,
+        o_clerk as clerk_name,
+        o_shippriority as ship_priority,
+        o_comment as comment
+
+    from source
+
 )
 
-select * from source_data
+select * from renamed
